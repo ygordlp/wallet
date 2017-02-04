8fe66dca45705ab7c148a81ffbe1e880e0bd7e46
alunor17 <alunor17@M72444.local>
2017-02-04 13:25:57 -0300

    Criado models, fragments e toda estrutura de pastas do app

---
+++ app/src/main/java/br/edu/unifor/ewallet/fragments/FragmentReceitas.java
@@ -0,0 +1,26 @@
+package br.edu.unifor.ewallet.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.edu.unifor.ewallet.R;
+
+/**
+ * Created by alunor17 on 04/02/17.
+ */
+
+public class FragmentReceitas extends Fragment {
+
+    private View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.fragement_receitas, container, false);
+        return rootview;
+    }
+}
