ca73eecee4d2ae10e348a220e72a2c83ed8f2611
alunor17 <alunor17@M72444.local>
2017-02-04 16:54:07 -0300

    Recycleview e cardview add e tela splash criada

---
+++ app/src/main/java/br/edu/unifor/ewallet/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package br.edu.unifor.ewallet.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.edu.unifor.ewallet.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
