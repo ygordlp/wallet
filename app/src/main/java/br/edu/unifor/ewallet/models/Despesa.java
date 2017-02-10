4f1ae0a7f11264b4f11cfc4ba8f1f229b8a1f0c8
alunor17 <alunor17@M72440.local>
2017-02-10 20:23:04 -0300

    Implementacao de cadastro de despesas

--- app/src/main/java/br/edu/unifor/ewallet/models/Despesa.java
+++ app/src/main/java/br/edu/unifor/ewallet/models/Despesa.java
@@ -1,5 +1,6 @@
 package br.edu.unifor.ewallet.models;
 
+import com.google.common.base.Strings;
 import com.orm.SugarRecord;
 
 /**
@@ -10,14 +11,26 @@ public class Despesa extends SugarRecord {
 
     private Conta conta;
     private TipoDespesa tipoDespesa;
+    private Double valor;
+    private Strings dataSaida;
+    private Strings descricao;
+    private boolean isPago;
+    private boolean isFixa;
+
+
 
     public Despesa() {
 
     }
 
-    public Despesa(Conta conta, TipoDespesa tipoDespesa) {
+    public Despesa(Conta conta, TipoDespesa tipoDespesa, Double valor, Strings dataSaida, Strings descricao, boolean isPago, boolean isFixa) {
         this.conta = conta;
         this.tipoDespesa = tipoDespesa;
+        this.valor = valor;
+        this.dataSaida = dataSaida;
+        this.descricao = descricao;
+        this.isPago = isPago;
+        this.isFixa = isFixa;
     }
 
     public Conta getConta() {
@@ -34,5 +47,45 @@ public class Despesa extends SugarRecord {
 
     public void setTipoDespesa(TipoDespesa tipoDespesa) {
         this.tipoDespesa = tipoDespesa;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Strings getDataSaida() {
+        return dataSaida;
+    }
+
+    public void setDataSaida(Strings dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public Strings getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(Strings descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isPago() {
+        return isPago;
+    }
+
+    public void setPago(boolean pago) {
+        isPago = pago;
+    }
+
+    public boolean isFixa() {
+        return isFixa;
+    }
+
+    public void setFixa(boolean fixa) {
+        isFixa = fixa;
     }
 }
