6ad60b93ce4ede93b526def849624af1796049fc
U-GRUPOGSH\rafael.comar <rafael.comar@gshdesenv01.grupogsh.com.br>
2017-02-10 15:36:56 -0200

    alteração da versao do support do appcompat e design de 24.2.1 para 24.1.0, por bug na exibição do icone na hora da validação dos campos do formulario.

--- app/src/main/java/br/edu/unifor/ewallet/activities/InserirContaActivity.java
+++ app/src/main/java/br/edu/unifor/ewallet/activities/InserirContaActivity.java
@@ -1,7 +1,10 @@
 package br.edu.unifor.ewallet.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,7 +22,7 @@ import br.edu.unifor.ewallet.models.TipoConta;
  * Created by Naldo on 2/4/2017.
  */
 
-public class InserirContaActivity extends AppCompatActivity {
+public class InserirContaActivity  extends AppCompatActivity implements TextWatcher {
 
     private Spinner mSpinTipoConta;
     private EditText mTxtBanco;
@@ -32,6 +35,7 @@ public class InserirContaActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ValiFi.install(this);
         setContentView(R.layout.inserir_conta);
 
         mSpinTipoConta = (Spinner) findViewById(R.id.spinTipoConta);
@@ -51,22 +55,50 @@ public class InserirContaActivity extends AppCompatActivity {
 
 
                 Log.i("teste", "onClick: " + mTxtBanco.getText().toString());
+
+
                 //TipoConta tipoConta, String banco, String numeroConta, String titulo, String cor, Double saldo
-                Conta conta = new Conta((TipoConta) mSpinTipoConta.getSelectedItem(),
-                        mTxtBanco.getText().toString(), mTxtNumConta.getText().toString(), mTxtTitulo.getText().toString(),
-                        mSpinCor.getSelectedItem().toString(), Double.parseDouble( mTxtSaldo.getText().toString()) );
+//                Conta conta = new Conta((TipoConta) mSpinTipoConta.getSelectedItem(),
+//                        mTxtBanco.getText().toString(), mTxtNumConta.getText().toString(), mTxtTitulo.getText().toString(),
+//                        mSpinCor.getSelectedItem().toString(), Double.parseDouble( mTxtSaldo.getText().toString()) );
 
-                ContaController contaController = new ContaController();
-                boolean result = contaController.verificarContaExistente(conta);
-                if (result == true){
-                    Toast.makeText(getApplicationContext(), "JA EXISTE", Toast.LENGTH_SHORT).show();
-                }else{
-                    contaController.inserirConta(conta);
-                    Toast.makeText(getApplicationContext(), "SALVO", Toast.LENGTH_SHORT).show();
 
+
+
+                if (mTxtTitulo.getText().toString().equals("")){
+                    mTxtTitulo.setError("ta errado");
+//
                 }
+
+//                ContaController contaController = new ContaController();
+//                boolean result = contaController.verificarContaExistente(conta);
+//                if (result == true){
+//                    Toast.makeText(getApplicationContext(), "JA EXISTE", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    contaController.inserirConta(conta);
+//                    Toast.makeText(getApplicationContext(), "SALVO", Toast.LENGTH_SHORT).show();
+//
+//                }
             }
         });
+
+
+
     }
 
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
 }
