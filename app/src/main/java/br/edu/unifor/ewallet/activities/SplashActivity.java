781ba7b129ed0274b191a8859da4f197e66a6437
maycon douglas <maycon@csktech.com.br>
2017-02-10 10:51:48 -0300

    Merge

--- app/src/main/java/br/edu/unifor/ewallet/activities/SplashActivity.java
+++ app/src/main/java/br/edu/unifor/ewallet/activities/SplashActivity.java
@@ -3,13 +3,11 @@ package br.edu.unifor.ewallet.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import br.edu.unifor.ewallet.R;
+
 
 public class SplashActivity extends AppCompatActivity {
 
