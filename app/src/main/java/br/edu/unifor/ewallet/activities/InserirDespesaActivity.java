ca2a34b4d3041f5b474929590b21c882ab091a00
maycon douglas <maycon@csktech.com.br>
2017-02-10 17:32:12 -0300

    Implementacao de cadastro de despesas

--- app/src/main/java/br/edu/unifor/ewallet/activities/InserirDespesaActivity.java
+++ app/src/main/java/br/edu/unifor/ewallet/activities/InserirDespesaActivity.java
@@ -1,8 +1,26 @@
 package br.edu.unifor.ewallet.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import br.edu.unifor.ewallet.R;
+
 /**
  * Created by maycon on 2/10/17.
  */
 
-public class InserirDespesaActivity {
+public class InserirDespesaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_despesa);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
 }
