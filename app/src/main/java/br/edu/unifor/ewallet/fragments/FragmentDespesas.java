1bd632bc141db6a2e4f88548b103c11030293118
alunor17 <alunor17@M72451.local>
2017-02-11 11:07:03 -0300

    listagem de despesas feita

--- app/src/main/java/br/edu/unifor/ewallet/fragments/FragmentDespesas.java
+++ app/src/main/java/br/edu/unifor/ewallet/fragments/FragmentDespesas.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import br.edu.unifor.ewallet.R;
+import br.edu.unifor.ewallet.adapters.ListaContaAdapter;
+import br.edu.unifor.ewallet.adapters.ListaDespesaAdapter;
+import br.edu.unifor.ewallet.controllers.DespesaController;
+import br.edu.unifor.ewallet.models.Conta;
+import br.edu.unifor.ewallet.models.Despesa;
 
 /**
  * Created by alunor17 on 04/02/17.
@@ -15,12 +21,21 @@ import br.edu.unifor.ewallet.R;
 
 public class FragmentDespesas extends Fragment {
 
+
     private View rootview;
+    private ListView listViewDespesas;
+    private ListaDespesaAdapter listaDespesaAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.fragment_despesas, container, false);
+        this.listViewDespesas = (ListView) rootview.findViewById(R.id.listViewDespesa);
+        this.listaDespesaAdapter = new ListaDespesaAdapter(DespesaController.getAll(), rootview.getContext());
+
+        this.listViewDespesas.setAdapter(this.listaDespesaAdapter);
+
+
         return rootview;
     }
 }
